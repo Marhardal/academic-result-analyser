e25bece3d1ebcc65d9e8ff52132b6088b1ab0074
Martin Harawa <martindharawa@gmail.com>
2026-06-21 20:24:05 +0200

    The file consist of basic functionality which loads student data, gets the head or first 5 rows, loads the scores only and gets the average score.

--- analyser.py
+++ analyser.py
@@ -4,8 +4,15 @@ file = "./Analyser.xlsx"
 # Loading the file in pandas
 load = pd.read_excel(file)
 # Loading the file content in a dataframe 
-df = pd.DataFrame(load)
+# df = pd.DataFrame(load)
 # showing the first 5 rows
 # print(df[0:5])
-# Getting rows and columns
-print(df.info())
+print(load.head())
+# Getting rows and columns count. Display the column names and data types.
+# print(df.info())
+# print(df.describe())
+# Average score across all students and all subjects.
+listscore = load[["Score"]]
+average = listscore.sum()/listscore.count()
+# print(average)
+print(listscore.mean())
