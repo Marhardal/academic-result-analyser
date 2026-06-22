ea598f9d3eb35a9b407ac24391641fea0e5cb543
Martin Harawa <martindharawa@gmail.com>
2026-06-22 17:05:39 +0200

    Added filtering for students who passed and Maths result only.

--- analyser.py
+++ analyser.py
@@ -7,12 +7,24 @@ load = pd.read_excel(file)
 # df = pd.DataFrame(load)
 # showing the first 5 rows
 # print(df[0:5])
-print(load.head())
+# print(load.head())
 # Getting rows and columns count. Display the column names and data types.
 # print(df.info())
 # print(df.describe())
 # Average score across all students and all subjects.
-listscore = load[["Score"]]
+listscore = load["Score"]
 average = listscore.sum()/listscore.count()
 # print(average)
 print(listscore.mean())
+
+df = pd.DataFrame(load)
+# Filter stodents who passed
+student_p = df[df["Passed"] == True]
+
+# print(student_p)
+# Filter Maths Result
+Maths_rst = df[df["Subject"] == "Math"]
+
+print(Maths_rst)
+
+
